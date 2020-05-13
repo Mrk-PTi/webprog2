ff44d5f63c05f02fb58370b780c98827fa151b2c
Mrk-PTi <markszalai22@gmail.com>
2020-05-13 11:15:43 +0200

    images.php feltöltése a protected mappába

---
+++ protected/images.php
@@ -0,0 +1,240 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <meta name="author" content="Project">
+
+      <title>Képek | Portfólió Weblap</title>
+      
+	  <link rel="stylesheet" href="../public/style.css">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+      <link href="https://fonts.googleapis.com/css?family=Roboto+Mono:100,100i,300,300i,400,400i,500,500i,700,700i" rel="stylesheet">
+      <script src="https://unpkg.com/ionicons@4.5.5/dist/ionicons.js"></script>
+      <link rel="stylesheet" href="../public/animate.css">
+      <script src="../wow.min.js"></script>
+
+</head>
+<body>
+      <div class="wrapper">
+
+            <nav>
+                  <span id="brand">
+                        <a href="../index.php">Project</a>
+                  </span>
+
+                  <ul id="menu">
+                        <li><a href="../index.php">Home<span>.</span></a></li>
+                        <li><a href="description.php">Description<span>.</span></a></li>
+                        <li><a href="about.php">About Me<span>.</span></a></li>
+                        <li><a href="contact.php">Contact<span>.</span></a></li>
+                  </ul>
+
+                  <div id="toggle">
+                        <div class="span">menu</div>
+                  </div>
+
+            </nav>
+
+            <div id="resize">
+                  <div class="close-btn">close</div>
+
+                  <ul id="menu">
+                        <li><a href="../index.php">Home<span>.</span></a></li>
+                        <li><a href="description.php">Description<span>.</span></a></li>
+                        <li><a href="about.php">About Me<span>.</span></a></li>
+                        <li><a href="contact.php">Contact<span>.</span></a></li>
+                  </ul>
+            </div>
+
+            <div class="whitespace"></div>
+
+            <div class="container">
+                  <div class="hero-content">
+                        <br><br>
+                        <div class="row">
+                        <div class="col-lg-8"></div>
+
+                        <div class="col-lg-4 project project1 wow fadeInUp" onclick="location.href='protected/images.php'"></div>
+						  </div>
+
+						  <div class="whitespace"></div>
+
+						  <div class="row">
+								<div class="col-lg-6 project project2 wow fadeInUp" onclick="location.href='protected/images.php'"></div>
+
+								<div class="col-lg-6"></div>
+						  </div>
+
+						  <div class="whitespace"></div>
+
+						  <div class="row">
+								<div class="col-lg-7"></div>
+
+								<div class="col-lg-5 project project3 wow fadeInUp" onclick="location.href='protected/images.php'"></div>
+
+								<div class="col-lg-1"></div>
+						  </div>
+
+						  <div class="whitespace"></div>
+
+						  <div class="row">
+								<div class="col-lg-1"></div>
+
+								<div class="col-lg-5 project project4 wow fadeInUp" onclick="location.href='protected/images.php'"></div>
+
+								<div class="col-lg-6"></div>
+						  </div>
+						  
+						  <div class="row">
+								<div class="col-lg-7"></div>
+
+								<div class="col-lg-4 project project5 wow fadeInUp" onclick="location.href='protected/images.php'"></div>
+
+								<div class="col-lg-1"></div>
+						  </div>
+
+						  <div class="whitespace"></div>
+
+						  <div class="row">
+								<div class="col-lg-1"></div>
+
+								<div class="col-lg-5 project project6 wow fadeInUp" onclick="location.href='protected/images.php'"></div>
+
+								<div class="col-lg-6"></div>
+						  </div>
+						  
+						  <div class="row">
+								<div class="col-lg-7"></div>
+
+								<div class="col-lg-4 project project7 wow fadeInUp" onclick="location.href='protected/images.php'"></div>
+
+								<div class="col-lg-1"></div>
+						  </div>
+
+						  <div class="whitespace"></div>
+
+						  <div class="row">
+								<div class="col-lg-1"></div>
+
+								<div class="col-lg-5 project project8 wow fadeInUp" onclick="location.href='protected/images.php'"></div>
+
+								<div class="col-lg-6"></div>
+						  </div>
+
+						  <div class="whitespace"></div>
+					</div>
+                  </div>
+            </div>
+			
+            <br><br>
+
+            <div class="whitespace"></div>
+
+            <div class="footer">
+                  <div class="container">
+                        <br><br>
+
+                        <div class="collab">
+                              <div class="row">
+                                    <div class="col-lg-12">
+                                          <p class="wow fadeInUp">A design és a kialakítás forrása: "CodeGrid" csatorna</p>
+                                    </div>
+                              </div>
+                        </div>
+
+                        <br>
+
+                        <div class="hr">
+                              <div class="row"></div>
+                        </div>
+
+                        <br><br>
+
+                        <div class="info">
+                              <div class="row">
+                                    <div class="col-lg-4" id="personal">
+                                          <p class="wow fadeInUp">Csatlakozz</p>
+                                          <h4 class="wow fadeInUp" data-wow-delay="0.2s">Insta: @project</h4>
+                                          <br><br>
+                                    </div>
+
+                                    <div class="col-lg-4" id="media">
+                                          <p class="wow fadeInUp" data-wow-delay="0s">Kövess</p>
+
+                                          <ul>
+                                                <li id="fb" class="wow fadeInUp" data-wow-delay="0.4s">FB</li>
+                                                <li id="ig" class="wow fadeInUp" data-wow-delay="0.6s">Insta</li>
+                                                <li id="tw" class="wow fadeInUp" data-wow-delay="0.8s">Twitter</li>
+                                          </ul>
+
+                                          <br><br>
+                                    </div>
+
+                                    <div class="col-lg-4" id="address">
+                                          <p class="wow fadeInUp" data-wow-delay="0s">Email</p>
+                                          <h4 class="wow fadeInUp" data-wow-delay="0.2s">project@gmail.com</h4>
+                                          <br><br>
+                                    </div>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+
+            <!--------------- footer ends here --------------->
+
+            <br><br><br><br>
+
+      </div>
+
+      <!-- greensock cdn -->
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/2.1.2/TweenMax.min.js"></script>
+
+      <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+      <script type="text/javascript">
+
+      // navigation starts here
+      $("#toggle").click(function() {
+            $(this).toggleClass('on');
+            $("#resize").toggleClass("active");
+      });
+
+      $("#resize ul li a").click(function() {
+            $(this).toggleClass('on');
+            $("#resize").toggleClass("active");
+      });
+
+      $(".close-btn").click(function() {
+            $(this).toggleClass('on');
+            $("#resize").toggleClass("active");
+      });
+
+      // navigation ends here
+
+      // nav animation
+
+      TweenMax.from("#brand", 1, {
+            delay: 0.4,
+            y: 10,
+            opacity: 0,
+            ease: Expo.easeInOut
+      })
+
+      TweenMax.staggerFrom("#menu li a", 1, {
+            delay: 0.4,
+            opacity: 0,
+            ease: Expo.easeInOut
+      }, 0.1);
+
+      // nav animation ends
+
+
+
+      new WOW().init();
+
+
+      </script>
+</body>
+</html>
